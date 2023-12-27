780036d45fc6ec3b30ff308c7a7a7d9efb8d6aef
misua <charlespogzz@googlemail.com>
2023-12-27 16:01:21 +0800

    testing loops

--- elevate/crm/views.py
+++ elevate/crm/views.py
@@ -5,26 +5,35 @@ from django.shortcuts import render
 
 def home(request):
 
-    clientlist =[
+    examList = [
         {
-            'id': 1,
-            'name': 'John wick',
-            'occupation': 'electrician',
-
+            'id':1,
+            'name':'Science',
+            'grade':'12',
         },
-         {
-            'id': 2,
-            'name': 'Smith wesson',
-            'occupation': 'musician',
-
-        }
+        {
+            'id':2,
+            'name':'Math',
+            'grade':'12',
+        },
+        {
+            'id':3,
+            'name':'English',
+            'grade':'12',
+        },
+        {
+            'id':4,
+            'name':'Nepali',
+            'grade':'12',
+        },
+        {
+            'id':5,
+            'name':'Social',
+            'grade':'12',
+        },
     ]
 
-
-    context = {
-        'Mainclientlist': clientlist
-    }
-
+    context = { 'exams': examList }
 
     return render(request, 'crm/index.html',context)
 
