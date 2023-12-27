8e4cea26734f306bf2ac659805504431a7fa57a5
misua <charlespogzz@googlemail.com>
2023-12-27 19:44:05 +0800

    creating models with fk to other tables

--- elevate/crm/models.py
+++ elevate/crm/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=1000)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+
+    reviewer_name = models.CharField(max_length=50)
+    review_title = models.CharField(max_length=100)
+
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.reviewer_name
--- elevate/crm/views.py
+++ elevate/crm/views.py
@@ -5,37 +5,7 @@ from django.shortcuts import render
 
 def home(request):
 
-    examList = [
-        {
-            'id':1,
-            'name':'Science',
-            'grade':'12',
-        },
-        {
-            'id':2,
-            'name':'Math',
-            'grade':'12',
-        },
-        {
-            'id':3,
-            'name':'English',
-            'grade':'12',
-        },
-        {
-            'id':4,
-            'name':'Nepali',
-            'grade':'12',
-        },
-        {
-            'id':5,
-            'name':'Social',
-            'grade':'12',
-        },
-    ]
-
-    context = { 'exams': examList }
-
-    return render(request, 'crm/index.html',context)
+    return render(request, 'crm/index.html')
 
 def register(request):
     return render(request, 'crm/register.html')
