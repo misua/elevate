1df7e1ad927769f7fb714e8dc6f21f94d8ec2a3b
misua <charlespogzz@googlemail.com>
2023-12-31 10:57:22 +0800

    wanted to look busy? :)

--- elevate/crm/views.py
+++ elevate/crm/views.py
@@ -85,7 +85,8 @@ def register(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponse('User created successfully')
+            return redirect('my-login')
+
         
     context = {'RegistrationForm': form}
     return render(request, 'crm/register.html', context )
@@ -97,16 +98,20 @@ def my_login(request):
     form = LoginForm()
 
     if request.method == 'POST':
-        form = LoginForm(request.POST)
+        form = LoginForm(request, data=request.POST)
     
         if form.is_valid():
-            username = request.POST['username']
-            password= request.POST['password']
-            user = authenticate(username=username, password=password)
+            username = request.POST.get('username')
+            password= request.POST.get('password')
+            user = authenticate(request,username=username, password=password)
 
             if user is not None:
-                login(request, user)
-                return redirect(dashboard)
+               login(request, user)
+               return redirect('dashboard')
+          
+            # else:
+            #     return redirect('my-login')
+        
     
     context = {'LoginForm': form}
 
